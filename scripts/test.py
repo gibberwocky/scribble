4b4d92ba92693b0abfd832a29a874a4abb62c5d4
gibberwocky <gibberwocky@googlemail.com>
2026-07-02 08:23:42 +0100

    Added test script

---
+++ scripts/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from pathlib import Path
+import scanpy as sc
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+from scribble.import_data import setup_environment
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--input_file", type=str, required=True)
+parser.add_argument("--plot_file", type=str, required=True)
+parser.add_argument("--markers", nargs="+", required=True)
+
+args = parser.parse_args()
+
+#marker_dict = ["TTR", "CP", "PAX8", "HNF1B", "DNAH11", "MAP2"]
+
+
+
+# Import adata
+adata = sc.read(args.input_file)
+
+
+
+sc.pl.dotplot(
+    adata,
+    args.markers,
+    standard_scale="var",
+    dendrogram=False
+)
+
+
+plt.savefig(args.plot_file, dpi=300, bbox_inches="tight")
+plt.close()
